254103983985792380d58a8ac856f58c1e73bc86
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2025-09-22 09:26:49 -0300

    We are portable now

---
+++ hosts/nixos/joker/default.nix
@@ -0,0 +1,50 @@
+{ lib, pkgs, ... }:
+
+with lib;
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  system.stateVersion = "25.05";
+
+  networking.hostName = "joker";
+
+  circus.nixos = {
+    base = enabled;
+    boot = enabled;
+    doas = enabled;
+    network = enabled;
+
+    hyprland = enabled;
+    sddm = enabled;
+
+    fonts = enabled;
+  };
+
+  home-manager.users.bruno = { ... }: {
+    home.stateVersion = "25.05";
+
+    circus.home = {
+      base = enabled;
+      hyprland = enabled;
+      hyprsleep = enabled;
+      alacritty = enabled;
+    };
+
+    home.packages = with pkgs; [
+      brave
+    ];
+  };
+
+#  hardware.graphics = {
+#    enable = true;
+#    enable32Bit = true;
+#    extraPackages = with pkgs; [
+#      mesa
+#      amdvlk
+#    ];
+#    extraPackages32 = with pkgs; [
+#      driversi686Linux.amdvlk
+#    ];
+#  };
+}
